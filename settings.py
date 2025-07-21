731a5580b1aabe95e1311e79344fa20035d41a86
Vilakati <nontobekovilakati@gmail.com>
2025-07-21 07:55:56 +0000

    Updated Django settings and project files

---
+++ settings.py
@@ -0,0 +1,17 @@
+import environ
+import os
+
+env = environ.Env()
+environ.Env.read_env()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
+}
+
